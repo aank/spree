decfa7b3b6cf7bd51de98eddc2db88b97a3893b3
Sean Schofield <schof@apache.org>
2009-08-05 10:17:42 -0400

    Fixed a few issues with editing shipping methods.

--- app/controllers/admin/shipping_methods_controller.rb
+++ app/controllers/admin/shipping_methods_controller.rb
@@ -14,10 +14,10 @@ class Admin::ShippingMethodsController < Admin::BaseController
   def build_object
     @object ||= end_of_association_chain.send((parent? ? :build : :new), object_params)
     @object.calculator = params[:shipping_method][:calculator_type].constantize.new if params[:shipping_method]
+    @object
   end
   
   def load_data
-    build_object
     @available_zones = Zone.find :all, :order => :name                      
     @calculators = Calculator.all_available_for(@object)
   end    
