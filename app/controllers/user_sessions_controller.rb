e7934fb38a6ac785db0bb5e7a8abe6bf15eba849
Sean Schofield <sean@railsdog.com>
2009-10-19 12:02:04 -0400

    Some feedback for case where user logs in via OpenID but email is missing still.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -76,6 +76,7 @@ class UserSessionsController < Spree::BaseController
         flash[:notice] = t(:user_created_successfully)
         redirect_back_or_default account_url
       else
+        flash[:notice] = t(:missing_required_information)
         redirect_to :controller => :users, :action => :new, :user => {:openid_identifier => @user.openid_identifier}
       end
     end
