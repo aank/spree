afd9a7579e6aae9f009ab479b0dbecf64120f238
Sean Schofield <sean@railsdog.com>
2011-09-04 14:13:17 -0400

    More progress on extension generator

--- core/lib/generators/spree_core/extension/extension_generator.rb
+++ core/lib/generators/spree_core/extension/extension_generator.rb
@@ -1,20 +1,41 @@
 require "rails/generators/rails/plugin/plugin_generator"
 
-module SpreeCore
-  module Generators
-    class ExtensionGenerator < Rails::Generators::PluginGenerator
+class SpreeCore::ExtensionGenerator < Rails::Generators::NamedBase
 
-      def self.source_paths
-        paths = self.superclass.source_paths
-        paths << File.expand_path('../templates', __FILE__)
-        paths.flatten
-      end
+  desc "Creates new Spree extension"
 
-      protected
-      def plugin_dir(join=nil)
-        file_name
-      end
+  source_root File.expand_path("../templates", __FILE__)
+  #def self.source_paths
+    #paths = self.superclass.source_paths
+    #paths << File.expand_path('../templates', __FILE__)
+    #paths.flatten
+  #end
 
-    end
+   def create_root_files
+    empty_directory file_name
+    empty_directory "#{file_name}/config"
+    empty_directory "#{file_name}/db"
+    empty_directory "#{file_name}/public"
+    template "LICENSE", "#{file_name}/LICENSE"
+    template "Rakefile", "#{file_name}/Rakefile"
+    template "README", "#{file_name}/README"
+    template "gitignore", "#{file_name}/.gitignore"
+    template "extension.gemspec", "#{file_name}/#{file_name}.gemspec"
+    template "Versionfile", "#{file_name}/Versionfile"
   end
+
+  def create_app_dirs
+    empty_directory "#{file_name}/app"
+    empty_directory "#{file_name}/app/controllers"
+    empty_directory "#{file_name}/app/helpers"
+    empty_directory "#{file_name}/app/models"
+    empty_directory "#{file_name}/app/views"
+    empty_directory "#{file_name}/spec"
+  end
+
+  protected
+  def plugin_dir(join=nil)
+    file_name
+  end
+
 end
