34c100f8a8548f59a92245477f2958696a668d4e
Sean Schofield <sean@railsdog.com>
2011-09-05 09:56:57 -0400

    More stuff related to extension generator

--- core/lib/generators/spree_core/extension/extension_generator.rb
+++ core/lib/generators/spree_core/extension/extension_generator.rb
@@ -15,7 +15,6 @@ class SpreeCore::ExtensionGenerator < Rails::Generators::NamedBase
     empty_directory file_name
     empty_directory "#{file_name}/config"
     empty_directory "#{file_name}/db"
-    empty_directory "#{file_name}/public"
     template "LICENSE", "#{file_name}/LICENSE"
     template "Rakefile", "#{file_name}/Rakefile"
     template "README", "#{file_name}/README"
@@ -26,12 +25,23 @@ class SpreeCore::ExtensionGenerator < Rails::Generators::NamedBase
 
   def create_app_dirs
     empty_directory "#{file_name}/app"
+    empty_directory "#{file_name}/app/assets"
     empty_directory "#{file_name}/app/controllers"
     empty_directory "#{file_name}/app/helpers"
     empty_directory "#{file_name}/app/models"
     empty_directory "#{file_name}/app/views"
     empty_directory "#{file_name}/spec"
   end
+
+  def create_lib_files
+    directory "lib", "#{file_name}/lib"
+  end
+
+  def misc_stuff
+    template "routes.rb", "#{file_name}/config/routes.rb"
+    template "spec_helper.rb", "#{file_name}/spec/spec_helper.rb"
+  end
+
 
   protected
   def plugin_dir(join=nil)
